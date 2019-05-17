3e39a0de42938da789d5b4b9d4cbc534f59c9afa
Tham <zarub2k@gmail.com>
2019-05-17 18:27:21 +0530

    timeout is added

--- src/main/java/com/hustlebar/microhustle/faulttolerance/FaultTolerance.java
+++ src/main/java/com/hustlebar/microhustle/faulttolerance/FaultTolerance.java
@@ -6,7 +6,22 @@ import javax.ws.rs.core.Response;
 @ApplicationScoped
 public class FaultTolerance implements IFaultTolerance {
     @Override
-    public Response timeout() {
-        return null;
+    public Response timeout(long sleep) {
+        System.out.println("Enters FaultTolerance.timeout()");
+
+        System.out.println("Before delay");
+        forcedDelay(sleep);
+        System.out.println("After delay");
+        return Response.ok()
+            .entity("FaultTolerance.timeout() success!")
+            .build();
+    }
+
+    private void forcedDelay(long sleep) {
+        try {
+            Thread.sleep(sleep);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Error inside forcedDelay()");
+        }
     }
 }
--- src/main/java/com/hustlebar/microhustle/faulttolerance/IFaultTolerance.java
+++ src/main/java/com/hustlebar/microhustle/faulttolerance/IFaultTolerance.java
@@ -1,12 +1,16 @@
 package com.hustlebar.microhustle.faulttolerance;
 
+import org.eclipse.microprofile.faulttolerance.Timeout;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 @Path("tolerance")
 public interface IFaultTolerance {
     @GET
     @Path("timeout")
-    Response timeout();
+    @Timeout(value = 10)
+    Response timeout(@QueryParam("sleep") long sleep);
 }
