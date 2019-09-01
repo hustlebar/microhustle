c7d366bd3f20a8d017e34c58efc8aa626a17f588
Tham <zarub2k@gmail.com>
2019-09-01 13:51:19 +0530

    Permutation code is added

---
+++ src/main/java/com/hustlebar/microhustle/trial/Permutation.java
@@ -0,0 +1,47 @@
+package com.hustlebar.microhustle.trial;
+
+import java.util.*;
+
+/**
+ * @author tham
+ */
+
+public class Permutation {
+    //123
+
+    List<String> values = new ArrayList<>(8);
+    Set<String> nonduplicates;
+
+    public void permute(String value) {
+        print(value, 0, value.length() - 1);
+
+        Collections.sort(values);
+        System.out.println(values);
+        nonduplicates = new TreeSet<>(values);
+//        System.out.println(nonduplicates);
+//        System.out.println(nonduplicates.toArray()[49]);
+    }
+
+    private void print(String value, int l, int r) {
+        if (l == r) {
+//            System.out.println(value);
+            values.add(value);
+        } else {
+            for (int i = l; i <= r; i++) {
+                value = swap(value, l, i);
+                print(value, l + 1, r);
+                value = swap(value, l, i);
+            }
+        }
+    }
+
+    private String swap(String value, int a, int b) {
+        char temp;
+        final char[] chars = value.toCharArray();
+        temp = chars[a];
+        chars[a] = chars[b];
+        chars[b] = temp;
+
+        return String.valueOf(chars);
+    }
+}
---
+++ src/test/java/com/hustlebar/microhustle/trial/PermutationTest.java
@@ -0,0 +1,15 @@
+package com.hustlebar.microhustle.trial;
+
+import org.junit.Test;
+
+/**
+ * @author tham
+ */
+
+public class PermutationTest {
+    private static final String value = "again";
+    @Test
+    public void testFind() {
+        new Permutation().permute(value);
+    }
+}
